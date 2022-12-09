9a5e09fdf83c3585e8d2c7f38b3c9df3a50b3848
BjornTr <seryygraf@gmail.com>
2022-12-09 17:55:07 +0200

    Added scanner

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,16 @@
 package app;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
+    static double num1;
+    static double num2;
     public static void main(String[] args) {
         System.out.println("My app for Users!");
         System.out.println("Version 1.0");
-        double num1 = 8;
-        double num2 = 2;
+        inputs();
         double c = add(num1, num2);
         System.out.println(c);
         double d = multiply(num1, num2);
@@ -31,5 +35,13 @@ public class Main {
 
     private static double subtract(double num1, double num2) {
         return num1 - num2;
+    }
+
+    private static void inputs() {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Please enter num 1: ");
+        num1 = scanner.nextDouble();
+        System.out.println("Please enter num 2: ");
+        num2 = scanner.nextDouble();
     }
 }
